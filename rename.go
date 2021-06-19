2560eb66344bd1a94e5d8d833d469463829399ee
Kumail Habib Mirza <kumailhabib12@gmail.com>
2021-06-19 22:16:12 +0500

    updated: rename only folders or only files using flags

--- rename.go
+++ rename.go
@@ -15,12 +15,25 @@ func checkError(err error) {
 	}
 }
 
+func rename(oldName string,folderPath *string, newName, fullPath string, 
+	counter *int) {
+	// attaching the full path with the new name
+	newPath := *folderPath + newName;
+	// renaming the files 
+	err := os.Rename(fullPath, newPath);
+	checkError(err);
+	*counter++;
+	fmt.Printf("%s\t%s\n", oldName, newName);
+}
+
 func main() {
 
 	// folder path to files that are going to be rename
 	folderPath := flag.String("folder", "~/Desktop", "folder path having files to rename");
 	// pattern of naming
-	pattern := flag.String("pattern", "pic", "set a pattern for renaming files",)
+	pattern := flag.String("pattern", "pic", "set a pattern for renaming files");
+	// if folders are to be renamed not files 
+	renameFolder := flag.Bool("f", false, "rename folders only within a directory");
 	flag.Parse();
 	
 	// reading files and folders of the given path
@@ -34,21 +47,48 @@ func main() {
 	fmt.Println("File Name\tNew Name");
 	//loop through the files
 	for _, f := range files {
+		
 		fullPath := *folderPath + f.Name();
-		// getting the file extension
-		ext :=  "." + strings.Split(f.Name(), ".")[1];
-		// constructing a new name
-		newName := *pattern + strconv.Itoa(counter) + ext;
-		// attaching the full path with the new name
-		newPath := *folderPath + newName;
-		// renaming the files 
-		err = os.Rename(fullPath, newPath);
-		checkError(err);
-		counter++;
 
-		fmt.Printf("%s\t%s\n", f.Name(), newName)
-		fileSlice = append(fileSlice, f.Name());
+		ext := "";
+		newName := "";
+
+		// if 'f' option is not given it means
+		// the current files should be renamed files with their original extension
+		if !*renameFolder {
+
+			// ignore directories within the folder
+			if f.IsDir(){
+				continue;
+			}
+			// getting the file extension
+			ext =  "." + strings.Split(f.Name(), ".")[1];
+			// constructing a new name for a file
+			newName = *pattern + strconv.Itoa(counter) + ext;
+
+			// rename 
+			rename(f.Name(), folderPath, newName, fullPath, &counter);
+			fileSlice = append(fileSlice, f.Name());
+	
+		}  else if *renameFolder {
+			// only rename the folders in the directory
+			if f.IsDir() {
+
+				// ignore normal files within the directory
+				if !f.IsDir() {
+					continue;
+				}
+
+				// constructing a new name for a folder
+				newName = *pattern + strconv.Itoa(counter);
+				// rename 
+				rename(f.Name(), folderPath, newName, fullPath, &counter);
+				fileSlice = append(fileSlice, f.Name());
+			}
+		}
+
 	}
+
 
 	fmt.Println("----------------------------------------");
 	fmt.Printf("Total Files Renamed: %d\n", len(fileSlice));	
