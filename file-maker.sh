015fa5482377820bea42382947bb4f27290f3539
Kumail Habib Mirza <kumailhabib12@gmail.com>
2021-07-18 14:53:51 +0500

    Added mode for creating folder and files using this script

--- file-maker.sh
+++ file-maker.sh
@@ -1,13 +1,26 @@
 #!/bin/bash 
 
 # create number given number of files of specified extension, and a random name
+# usage: ./file-maker.sh $1 $2 $3
+# $1: number of files and folders
+# $2: extension of the create file, leave empty for files without extension
+# $3: create a folder or a file
 
 # how to many files to create
 TIMES=$1
+# create files or folder
+mode=$2
 # echo "Running file maker" $TIMES "times"
 # get the specified extension if any
-ext=".${2}"
+ext=".${3}"
 
+
+# if mode is empty then set mode as file
+# meaning create file
+if [ $mode == "" ]; then 
+    echo "No mode given. Setting mode to file"
+    $mode="file"
+fi
 
 # print out the information passed as arguments
 echo "Creating " $TIMES " Files"
@@ -16,6 +29,7 @@ if [ $ext == "." ]; then
 else
     echo "Extension: " $ext
 fi
+echo "Mode: " $mode
 
 echo
 # check if a test/ folder exists
@@ -30,8 +44,7 @@ else
     echo
 fi
 
-
-
+# create a random files/folders in test/
 for (( c=0; c<=$TIMES; c++ ))
 do
     # create a random string of a file, having characters a-f 0-9 and of 5 len
@@ -46,9 +59,17 @@ do
         filename="${name}${ext}"
     fi
 
-    echo "Creating: test/"$filename
-    # create the file in the test folder
-    touch test/$filename
-    echo "File Created: test/"$filename 
+    # check the mode of opertion
+    if [[ $mode == "file" ]]; then
+        echo "Creating: test/"$filename
+        touch test/$filename
+        echo "File Created: test/"$filename 
+    # if mode is given as folder
+    else
+        foldername="${name}"
+        echo "Creating: test/"$foldername
+        mkdir -p test/$foldername
+        echo "Folder Created: test/"$foldername
+    fi 
     echo
 done
